756a6b74f6ad935d3e7c3da9323ac50c7297fb64
Jerry Sanguyo Jr <jsanguyo1624@gmail.com>
2025-04-18 14:56:10 +0800

    added: question backend

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -6,7 +6,7 @@ use App\Models\Question;
 use App\DataTables\CmsDataTable;
 use App\Http\Requests\QuestionRequest;
 use App\Services\QuestionService;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class QuestionController extends Controller
 {
@@ -30,33 +30,46 @@ class QuestionController extends Controller
             'data'
         ));
     }
-    
-    public function create()
+
+    public function store(QuestionRequest $request)
     {
-        //
+        $question = $this->questionServices->store($request->validated());
+
+        activity()
+            ->perfomedOn($question)
+            ->causedBy(Auth::user())
+            ->log('Added a question: '. $question->name);
+
+        return redirect()
+            ->route(Auth::user()->getRoleNames()->first() . '.question.index')
+            ->with('Question added successfully!');
     }
     
-    public function store(Request $request)
+    public function update(QuestionRequest $request, Question $question)
     {
-        //
-    }
-    
-    public function show(Question $question)
-    {
-        //
-    }
-    
-    public function edit(Question $question)
-    {
-        //
-    }
-    public function update(Request $request, Question $question)
-    {
-        //
+        $question = $this->questionServices->update($request->validated(), $question);
+
+        activity()
+            ->performedOn($question)
+            ->causedBy(Auth::user())
+            ->log('Updated a question: '.$question->name);
+
+        return redirect()
+            ->route(Auth::user()->getRoleNames()->first() . '.question.index')
+            ->with('Question updated successfully!');
     }
     
     public function destroy(Question $question)
     {
-        //
+        $question = $this->questionServices->destroy($question);
+
+        activity()
+            ->performedOn($question)
+            ->causedBy(Auth::user())
+            ->log('Deleted a question: '.$question->name);
+
+        return redirect()
+            ->route(Auth::user()->getRoleNames()->first() . '.question.index')
+            ->with('Question deleted successfully!');
     }
 }
